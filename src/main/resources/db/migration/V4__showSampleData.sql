eefee9cc7dc584ce63375aff238e890b3cf1f114
vinayakrdeshpande <15637546+vinayakrdeshpande@users.noreply.github.com>
2022-11-20 17:40:30 +0530

    sql update

--- src/main/resources/db/migration/V4__showSampleData.sql
+++ src/main/resources/db/migration/V4__showSampleData.sql
@@ -1,6 +1,6 @@
 
-INSERT INTO show VALUES ( 'DDLJ', '2022-11-20 00:00:00', '09:00', '12:00', 'HINDI', 'ROMANTIC', 250);
-INSERT INTO show VALUES ( 'DDLJ', '2022-11-20 00:00:00', '09:00', '12:00', 'KANNADA', 'ROMANTIC', 250);
-INSERT INTO show VALUES ( 'DDLJ', '2022-11-20 00:00:00', '09:00', '12:00', 'MARATHI', 'ROMANTIC', 250);
-INSERT INTO show VALUES ( 'SAIRAT', '2022-11-20 00:00:00', '12:00', '3:00', 'MARATHI', 'ROMANTIC', 250);
-INSERT INTO show VALUES ( 'JOURNEY TO MYSTERIOUS ISLAND', '2022-11-20 00:00:00', '12:00', '3:00', 'KANNADA', 'ADVENTURE', 250);
+INSERT INTO show ( movie, releasedate, starttime, endtime, language, genere, price) VALUES ( 'DDLJ', '2022-11-20 00:00:00', '09:00', '12:00', 'HINDI', 'ROMANTIC', 250);
+INSERT INTO show ( movie, releasedate, starttime, endtime, language, genere, price) VALUES ( 'DDLJ', '2022-11-20 00:00:00', '09:00', '12:00', 'KANNADA', 'ROMANTIC', 250);
+INSERT INTO show ( movie, releasedate, starttime, endtime, language, genere, price) VALUES ( 'DDLJ', '2022-11-20 00:00:00', '09:00', '12:00', 'MARATHI', 'ROMANTIC', 250);
+INSERT INTO show ( movie, releasedate, starttime, endtime, language, genere, price) VALUES ( 'SAIRAT', '2022-11-20 00:00:00', '12:00', '3:00', 'MARATHI', 'ROMANTIC', 250);
+INSERT INTO show ( movie, releasedate, starttime, endtime, language, genere, price) VALUES ( 'JOURNEY TO MYSTERIOUS ISLAND', '2022-11-20 00:00:00', '12:00', '3:00', 'KANNADA', 'ADVENTURE', 250);
